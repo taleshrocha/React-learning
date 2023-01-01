ec0800dee457e100dab8b5fbcdfd2095b08e9564
taleshrocha <taleshrochaz@gmail.com>
2023-01-01 12:20:26 -0300

    Hooks at a Glance

---
+++ hooks/state-hooks/src/App.js
@@ -0,0 +1,12 @@
+import { StateHook } from './components/StateHook'
+import { EffectHook } from './components/EffectHook'
+
+function App() {
+  return (
+    <div>
+      <EffectHook />
+    </div>
+  );
+}
+
+export default App;
