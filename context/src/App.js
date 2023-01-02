a9b1acca9e090d8176d7c26601437288250fb6bb
taleshrocha <taleshrochaz@gmail.com>
2023-01-02 11:08:41 -0300

    updating-the-ui-with-javascript

---
+++ context/src/App.js
@@ -0,0 +1,19 @@
+class App extends React.Component {
+  render() {
+    return <Toolbar theme="dark" />;
+  }
+}
+
+function Toolbar(props) {
+  return (
+    <div>
+      <ThemedButton theme={props.theme} />
+    </div>
+  );
+}
+
+class ThemedButton extends React.Component {
+  render() {
+    return <Button theme={this.props.theme} />;
+  }
+}
