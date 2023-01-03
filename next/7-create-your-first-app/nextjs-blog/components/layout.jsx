8cbeff489a592760639c81910b96f48d8aa79662
taleshrocha <taleshrochaz@gmail.com>
2023-01-03 15:02:56 -0300

    Dinamic routing

--- next/7-create-your-first-app/nextjs-blog/components/layout.jsx
+++ next/7-create-your-first-app/nextjs-blog/components/layout.jsx
@@ -56,7 +56,7 @@ export default function Layout({ children, home }) {
             </Link>
 
             <h2 className={utilStyles.headingLg}>
-              <Link href="/" className={utilStylescolorInherit}>
+              <Link href="/" className={utilStyles.colorInherit}>
                 {name}
               </Link>
             </h2>
---
+++ next/7-create-your-first-app/nextjs-blog/pages/posts/[id].jsx
@@ -0,0 +1,39 @@
+import Layout from '../../components/layout'
+import { getAllPostsIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
+import Date from '../../components/date'
+
+export async function getStaticPaths() {
+  const paths = getAllPostsIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id)
+
+  return {
+    props: {
+      postData
+    }
+  }
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      <Date dateString={postData.date} />
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  )
+}
